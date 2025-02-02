4c3e2bbad6426e48b0ff95cbd58de2f5d29494fb
Revan19501 <4.ak@bk.ru>
2025-02-02 09:08:41 +0300

    Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -36,7 +36,7 @@ public class Util {
         properties.setProperty("hibernate.connection.username", "root");
         properties.setProperty("hibernate.connection.password", "1234");
         properties.setProperty("hibernate.show_sql", "com.mysql.jdbc.Driver");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect"); 
         properties.setProperty("hibernate.hbm2ddl.auto", "update");
 
         Configuration configuration = new Configuration().addAnnotatedClass(User.class);
